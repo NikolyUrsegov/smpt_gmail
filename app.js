b2a86557fc5a84c8a19ec21f594d955ae5496df7
NikolyUrsegov <ursegovnikolaj@gmail.com>
2022-11-06 17:37:57 +0300

    test

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const bodyParser = require('body-parser')
 
 
 const app = express();
-const whitelist = ['http://localhost:3000', 'https://ttktest.vercel.app/']; //white list consumers
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(null, false);
-        }
-    },
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    optionsSuccessStatus: 200,
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
-};
-app.use(cors(corsOptions))
+// const whitelist = ['http://localhost:3000', 'https://ttktest.vercel.app/']; //white list consumers
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(null, false);
+//         }
+//     },
+//     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+// };
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
